cd25ecdb16a53f5b365ac35a68c03052b7145c29
MohammadAlMokdad <mhmdmkdd25@gmail.com>
2025-03-21 00:26:37 +0200

    initial commit

---
+++ WebApplication1/Student.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1
+{
+    public class Student
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+    }
+}
